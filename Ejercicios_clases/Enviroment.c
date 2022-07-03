a650495d9bfb3db9719f350c457fd094c7ccde4d
Elias Upstein <eliasupstein@gmail.com>
2022-07-03 01:05:13 -0300

    Correcciones

--- Ejercicios_clases/Enviroment.c
+++ Ejercicios_clases/Enviroment.c
@@ -4,23 +4,30 @@
 
 int main(int argc,char *argv[],char *envp[]) 
 {
-    int i=0;
+    // int i=0;
     char contenido[N];
     char *direccion;
 
-    while(envp[i]!=NULL) {
-        printf("%s\n", envp[i]);
-        i++;
-    }
-    for(i=0;envp[i]!=NULL;i++)
+    // while(envp[i]!=NULL) {
+    //     printf("%s\n", envp[i]);
+    //     i++;
+    // }
+    //Imprime todos los strings de enviroment
+    // for (int i = 0; envp[i]!=NULL; i++)
+    //     printf("%s\n", envp[i]);
+    
+    for(int i = 0; envp[i] != NULL; i++)
     {
-        if(strstr(envp[i],"LOGNAME")!=NULL)
+        printf("%s\n", envp[i]);                //Imprime todos los strings de enviroment
+
+        //if(strstr(envp[i],"LOGNAME") != NULL) //Si el string contiene LOGNAME  (Linux)
+        if(strstr(envp[i],"USERNAME") != NULL)  //Si el string contiene USERNAME (Windows)
         {
-            strcpy(contenido,envp[i]);
-            direccion = strtok(contenido,"=");
-            direccion = strtok(NULL,"=");
+            strcpy(contenido,envp[i]);          //Copia el string en otra variable
+            direccion = strtok(contenido,"=");  //Descarta la parte anterior al igual
+            direccion = strtok(NULL,"=");       //Guarda la dirección de lo siguiente al resultado
         }
     }
-    printf("%s\n",direccion);
+    printf("\nBienvenido: %s\n",direccion);       //Imprime el string obtenido
     return 0;
 }
--- Ejercicios_clases/Tipo_parcial3.c
+++ Ejercicios_clases/Tipo_parcial3.c
@@ -21,33 +21,55 @@ int main(int cantArg, char *args[])
     char palabras[N];
     char *direcciones[10];
     int count = 0;
+    int cantpalabras = 0;
 
     if (cantArg > 1)
     {
+        //Con espacios
         printf("Ingrese 10 palabras prohibidas:\n");
         fflush(stdin);
         fgets(palabras, N, stdin);  //¿Cómo controlo que ingrese realmente 10 palabras? (me lo corta el for)
+        if( palabras[strlen(palabras) - 1] == '\n')     //Borro el \n final para poder hacer la comparación
+            palabras[strlen(palabras) - 1] = '\0';
 
         //Guardo en direcciones el puntero a cada string
         direcciones[0] = strtok(palabras," ");
-        for (int i = 1; i < 10; i++)
-            direcciones[i] = strtok(NULL," ");  
-        
+        if (direcciones[0] != NULL)
+        {
+            cantpalabras++;
+            for (int i = 1; i < 10; i++)
+            {
+                direcciones[i] = strtok(NULL," ");  
+                if (direcciones[i] == NULL)
+                    break;
+                cantpalabras++;
+            }
+        }
         //Otra forma con scanf
         // printf("Ingrese 10 palabras prohibidas:\n");
         // fflush(stdin);
-        // scanf("%s %s %s %s %s %s %s %s %s %s", )  tendría que declarar 10 strings?
+        // scanf("%s %s %s %s %s %s %s %s %s %s", direcciones[0],direcciones[1],direcciones[2],direcciones[3],direcciones[4],direcciones[5],direcciones[6],direcciones[7],direcciones[8],direcciones[9],)  
 
+        //Con enter's
+        // printf("Ingrese 10 palabras prohibidas:\n");
+        // for (int i = 0; i < 10; i++)
+        // {
+        //     fflush(stdin);
+        //     fgets(direcciones[i], N, stdin);
+        //     //Si se ingresó solo un enter
+        //     if (strcmp(direcciones[i],"\n") == 0)
+        //         break; 
+        // }
         for (int i = 1; i < cantArg; i++)           //Para cada string pasado como argumento
         {
-            for (int j = 0; i < 10; j++)            //Para cada palabra ingresada como prohibida
+            for (int j = 0; j < 10 && j < cantpalabras; j++)//Para cada palabra ingresada como prohibida
             {
                 if (strcmp(args[i], direcciones[j]) == 0)       //Si los string son iguales
                 {
-                    for (int k = 0; k < strlen(args[i]); i++)   //Para cada letra del sting escribe X
+                    for (int k = 0; k < strlen(args[i]); k++)   //Para cada letra del sting escribe X
                     {
-                            args[i][k] = 'X';
-                            count++;
+                        args[i][k] = 'X';
+                        count++;
                     }
                 }
             }
@@ -55,8 +77,11 @@ int main(int cantArg, char *args[])
 
         printf("Mensaje moderado:\n");
         for (int i = 1; i < cantArg; i++)
+        {
             fputs(args[i], stdout);
-        printf("Cantidad de caracteres reemplazados: %d\n", count);
+            printf(" ");
+        }
+        printf("\nCantidad de caracteres reemplazados: %d\n", count);
     }
     else
         printf("No se ha ingresado ningún mensaje.\n");
