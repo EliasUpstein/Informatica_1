5ba5d2249a5610861c4e2f3ffb7c377a982c6a81
Elias Upstein <eliasupstein@gmail.com>
2022-09-03 09:47:25 -0300

    Ejercicio 4, tp7

--- Guía_de_TPs_2013/TP7/Ejercicio7_4.c
+++ Guía_de_TPs_2013/TP7/Ejercicio7_4.c
@@ -1,4 +1,34 @@
 /* Escriba una función que reciba un ángulo expresado en radianes y lo devuelva en
 formato sexagesimal.*/
 
-//Como estructura grados-minutos-segundos???
+struct sexagesimal
+{
+    int grados;
+    int minutos;
+    float segundos;
+};
+
+#define PI 3.1415
+
+#include <stdio.h>
+
+int main(void)
+{
+    float rad;
+    float aux;
+    struct sexagesimal sex;
+
+    printf("Ingrese un grado en radianes: ");
+    fflush(stdin);
+    scanf("%f", &rad);
+
+    aux = (rad * 180) / PI;
+
+    sex.grados = (int) aux;                                 //Almacena la parte entera del ángulo
+    sex.minutos = (int) (aux - sex.grados) * 60;            //Almacena la parte entera de los minutos
+    sex.segundos = (((aux - sex.grados) * 60) - sex.minutos) * 60;
+
+    printf("El ángulo ingresado en grados sexagesimales es: %d° %d' %f''\n", sex.grados, sex.minutos, sex.segundos);
+
+    return 0;
+}
